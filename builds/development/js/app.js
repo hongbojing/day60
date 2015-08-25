bfd2e50497a34b5ab1db62d04ee8445bd129380f
Hongbo Jing <jack.jing7@gmail.com>
2015-08-25 00:44:58 -0400

    test angular bootstrap ui and re-write in john papa style

---
+++ builds/development/js/app.js
@@ -0,0 +1,30 @@
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('AccordionDemoCtrl', AccordionDemoCtrl);
+
+function AccordionDemoCtrl() {
+  var vm=this;
+  vm.oneAtATime = true;
+
+  vm.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  vm.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  vm.addItem = function() {
+    var newItemNo = vm.items.length + 1;
+    vm.items.push('Item ' + newItemNo);
+  };
+
+  vm.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+}
